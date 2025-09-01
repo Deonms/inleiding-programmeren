80baae0a68b9e0095f75958ad037ca479f333d58
DeonsGaming <Deon.mob@gmail.com>
2025-09-01 14:23:38 +0200

    changes

---
+++ Vlaggen.java
@@ -0,0 +1,34 @@
+size(250,250);
+background(206,206,206);
+
+stroke( 255, 0, 0); 
+strokeWeight(10); 
+rect(10,10,75,10);
+stroke( 255, 255, 255); 
+strokeWeight(10); 
+rect(10,30,75,10);
+stroke( 0, 0, 255); 
+strokeWeight(10); 
+rect(10,50,75,10);
+
+
+stroke( 0, 0, 0); 
+strokeWeight(10); 
+rect(10,100,10,75);
+stroke( 255, 222, 89); 
+strokeWeight(10); 
+rect(30,100,10,75);
+stroke( 255, 0, 0); 
+strokeWeight(10); 
+rect(50,100,10,75);
+
+stroke( 255, 0, 0); 
+strokeWeight(10); 
+rect(10,200,75,10);
+stroke( 255, 255, 255); 
+strokeWeight(10); 
+rect(10,220,75,10);
+stroke( 93, 226, 231); 
+strokeWeight(10); 
+rect(10,240,75,10);
+
