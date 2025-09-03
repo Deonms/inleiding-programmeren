51c4aeca4b8ab5ced6a430adf95de3017fec529f
DeonsGaming <Deon.mob@gmail.com>
2025-09-03 10:07:00 +0200

    Update H05 Tekst Variabelen.java

---
+++ H05 Tekst Variabelen.java
@@ -0,0 +1,27 @@
+int d = 15;
+int c = 18;
+int Totaalsom = 0;
+
+String text = "dit is een getal als je 15 + 18 doet = ";
+String Totaal = " ";
+
+Totaalsom = d + c;
+
+Totaal = text + Totaalsom;
+
+println(Totaal);
+
+int dc = 33;
+int eindesom = 0;
+
+String bericht  = "nu eind getal keer dc (33) = ";
+String eindantwoord = " ";
+
+eindesom = Totaalsom * dc;
+
+eindantwoord = bericht + eindesom;
+
+println(eindantwoord);
+
+
+ 
