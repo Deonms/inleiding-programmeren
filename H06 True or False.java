3ff2590ad67a7c6056fedb8f63f303669a09aed4
DeonsGaming <Deon.mob@gmail.com>
2025-09-03 10:27:07 +0200

    Create H06 True or False.java

---
+++ H06 True or False.java
@@ -0,0 +1,32 @@
+int temperatuur = -2;
+if( temperatuur < 0) {
+println(" " + true);
+}
+if( temperatuur > 0) {
+println(" " + false);
+}
+
+String sneeuw = "Wit";
+if(sneeuw != "Geel"){
+println(" " + true);
+}
+if(sneeuw == "Geel"){
+println(" " + false);
+}
+
+int jantje = 6;
+if(jantje <= 4){
+println(" " + true);
+}
+if(jantje >= 4){
+println(" " + false);
+}
+
+String datum = "3-9";
+String mijnVerjaardag = "16-10";
+if(mijnVerjaardag == datum){
+println(" " + true);
+}
+if(mijnVerjaardag != datum){
+println(" " + false);
+}
