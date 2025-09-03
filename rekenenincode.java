02d8993cef7da43b77a445789c464a2f3fc077e6
DeonsGaming <Deon.mob@gmail.com>
2025-09-03 09:36:04 +0200

    Create rekenenincode.java

---
+++ rekenenincode.java
@@ -0,0 +1,31 @@
+size(250,250);
+
+int a = 5;
+int b = 10;
+int totaal = 0; 
+
+totaal = a + b;
+println(totaal);
+int c = totaal;
+
+totaal = a - b;
+println(totaal);
+int d = totaal;
+
+totaal = a * b;
+println(totaal);
+int e = totaal;
+
+totaal = a / b;
+println(totaal);
+int f = totaal;
+
+println(totaal);
+totaal = 5 + 3;
+totaal = 5 - 3;
+totaal = 5 * 3;
+totaal = 5 / 3;
+background(0,255,0);
+rect(c,b,e,f);
+
+println(ga meer code later toevoegen)
