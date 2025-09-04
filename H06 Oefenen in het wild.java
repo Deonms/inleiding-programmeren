879c617f779c59a18cd26b980bd04a061d2d75c7
DeonsGaming <Deon.mob@gmail.com>
2025-09-04 11:46:12 +0200

    Create H06 Oefenen in het wild.java

---
+++ H06 Oefenen in het wild.java
@@ -0,0 +1,133 @@
+int leeftijd = 15; //voer leeftijd in van klant voor prijs
+int temperatuur = 22; //voer hier de graden in van wat het Maximaal wordt vandaag
+if ( leeftijd <= 1) {
+  println("baby, " + "Toegangs  prijs = Gratis" );
+}
+if (leeftijd == 1) {
+  println("Druemes = Toegangsprijs = 2,50");
+}
+if (leeftijd == 2) {
+  println("Peuter = Toegangsprijs = 3,50");
+} else if (leeftijd == 3) {
+  println("Peuter = Toegangsprijs = 3,50");
+}
+if (leeftijd == 4) {
+  println("Kleuter = Toegangsprijs = 4,50");
+} else if (leeftijd == 5) {
+  println("Kleuter = Toegangsprijs = 4,50");
+}
+if (leeftijd == 6) {
+  println("Schoolkind = Toegangsprijs = 5,50");
+} else if (leeftijd == 7) {
+  println("Schoolkind = Toegangsprijs = 5,50");
+} else if (leeftijd == 8) {
+  println("Schoolkind = Toegangsprijs = 5,50");
+} else if (leeftijd == 9) {
+  println("Schoolkind = Toegangsprijs = 5,50");
+} else if (leeftijd == 10) {
+  println("Schoolkind = Toegangsprijs = 5,50");
+}
+if (leeftijd == 11) {
+  println("Puber = Toegangsprijs = 7,50");
+} else if (leeftijd == 12) {
+  println("Puber = Toegangsprijs = 7,50");
+} else if (leeftijd == 13) {
+  println("Puber = Toegangsprijs = 7,50");
+} else if (leeftijd == 14) {
+  println("Puber = Toegangsprijs = 7,50");
+} else if (leeftijd == 15) {
+  println("Puber = Toegangsprijs = 7,50");
+}
+if (leeftijd == 16) {
+  println("Adolescent = Toegangsprijs = 9,50");
+} else if (leeftijd == 17) {
+  println("Adolescent = Toegangsprijs = 9,50");
+}
+if ( leeftijd >= 18) {
+  println("18+, " + "Toegangs  prijs = 15,-" );
+}
+if ( leeftijd >= 65) {
+  println("Bejaarde, " + "25% korting" );
+}
+if ( leeftijd >= 65) {
+  String kortingleeftijd = "25% korting";
+  int kortingvanleeftijd;
+  kortingvanleeftijd = 15 / 4 * 3;
+  println("Nieuwe Prijs voor Bejaarde = " + kortingvanleeftijd + ",-");
+}
+{
+  println(" ");
+
+  println("10% korting bij 30+ graden celcius");
+}
+
+
+if ( temperatuur < 30) {
+  println("het is kouder dan 30 graden celcius = " + "geen korting");
+}
+if ( temperatuur >= 30) {
+  println("het is warmer dan 30 graden celcius = " + "Iedereen krijgt 10% korting");
+}
+if ( temperatuur >= 30);
+if ( leeftijd == 1);
+{
+  println("Nieuwe Prijs voor Druemes = " +"2,25");
+}
+{
+  if ( temperatuur >= 30);
+  if ( leeftijd == 2);
+  else if  ( leeftijd == 3);
+  {
+    println("Nieuwe Prijs voor Pueter = " +"3,15");
+  }
+}
+{
+  if ( temperatuur >= 30);
+  if ( leeftijd == 4);
+  else if  ( leeftijd == 5);
+  {
+    println("Nieuwe Prijs voor Kleuter = = " +"4,05");
+  }
+}
+{
+  if ( temperatuur >= 30);
+  if ( leeftijd == 6);
+  else if  ( leeftijd == 7);
+  else if  ( leeftijd == 8);
+  else if  ( leeftijd == 9);
+  else if  ( leeftijd == 10);
+  {
+    println("Nieuwe Prijs voor Schoolkind = = " +"4,95");
+  }
+}
+{
+  if ( temperatuur >= 30);
+  if ( leeftijd == 11);
+  else if  ( leeftijd == 12);
+  else if  ( leeftijd == 13);
+  else if  ( leeftijd == 14);
+  else if  ( leeftijd == 15);
+  {
+    println("Nieuwe Prijs voor Puber = = " +"6,75");
+  }
+}{
+  if ( temperatuur >= 30);
+  if ( leeftijd == 16);
+  else if  ( leeftijd == 17);
+  {
+    println("Nieuwe Prijs voor Adolescent = = " +"8,55");
+  }
+}{
+if ( temperatuur >= 30);
+if ( leeftijd >= 18);
+{
+  println("Nieuwe Prijs voor Volwassenen = " +"13,50");
+}
+}
+{
+if ( temperatuur >= 30);
+if ( leeftijd >= 65);
+{
+  println("Nieuwe Prijs voor Volwassenen = " +"8,10");
+}
+}
