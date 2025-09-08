35957b9a834d113250544360db87bc2222e4d051
DeonsGaming <Deon.mob@gmail.com>
2025-09-08 13:14:28 +0200

    Create H06 oefenen in het wild geslaagd.java

---
+++ H06 oefenen in het wild geslaagd.java
@@ -0,0 +1,75 @@
+int goedSlechtCijfer;
+int goedSlechtPresentie;
+int geslaagdOfGezakt; 
+
+float eindCijfer;
+eindCijfer = 7;
+if (eindCijfer < 5.5) {
+  println(eindCijfer);
+  println("Niet Voldoende");
+  goedSlechtCijfer = 1;
+}
+else if (eindCijfer >= 5.5); {
+  println(eindCijfer);
+  println("Voldoende");
+  goedSlechtCijfer = 2;
+}
+
+
+float Presentie;
+Presentie = 17;
+Presentie = Presentie * 5 ;
+if (Presentie < 80) {
+  println(Presentie + "%");
+  println("Presentie is te weinig");
+  goedSlechtPresentie = 1;
+}
+else if (Presentie >= 80); {
+  println(Presentie + "%");
+  println("Presentie is goed");
+  goedSlechtPresentie = 2;
+}
+
+geslaagdOfGezakt = goedSlechtPresentie % goedSlechtCijfer;
+if (geslaagdOfGezakt == 0) {
+  println("[Naam] is Geslaagd");
+}
+else if (geslaagdOfGezakt >= 1) {
+  println("[Naam] is Gezakt");
+}
+//Nieuw persoon
+println(" ");
+eindCijfer = 5.5;
+if (eindCijfer < 5.5) {
+  println(eindCijfer);
+  println("Niet Voldoende");
+  goedSlechtCijfer = 1;
+}
+else if (eindCijfer >= 5.5); {
+  println(eindCijfer);
+  println("Voldoende");
+  goedSlechtCijfer = 2;
+}
+
+
+Presentie = 16;
+Presentie = Presentie * 5 ;
+if (Presentie < 80) {
+  println(Presentie + "%");
+  println("Presentie is te weinig");
+  goedSlechtPresentie = 1;
+}
+else if (Presentie >= 80); {
+  println(Presentie + "%");
+  println("Presentie is goed");
+  goedSlechtPresentie = 2;
+}
+
+
+geslaagdOfGezakt = goedSlechtPresentie % goedSlechtCijfer;
+if (geslaagdOfGezakt == 0) {
+  println("[Naam] is Geslaagd");
+}
+else if (geslaagdOfGezakt >= 1) {
+  println("[Naam] is Gezakt");
+}
