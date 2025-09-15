d5e100c282c908a24c81f986eea4f9e4ae7123a4
DeonsGaming <Deon.mob@gmail.com>
2025-09-15 11:09:48 +0200

    Update H08 opdracht.java

--- H08 opdracht.java
+++ H08 opdracht.java
@@ -1,8 +1,8 @@
 int teller = 0;
 float leeftijd = 15;
-leeftijd = leeftijd * 365 * 24 * 60 * 60;
+leeftijd = leeftijd * 365 * 24;
 while(teller <= leeftijd){
-println("ik ga net zo lang door tellen tot en met je leeftijd in seconden en dat is" + leeftijd);
+println("ik ga net zo lang door tellen tot en met je leeftijd in uren.");
 println("teller is nu " + teller);
 teller++;
 
