e335a8651e5f82d80cac1dad5cdf24145a590bde
DeonsGaming <Deon.mob@gmail.com>
2025-09-15 11:48:49 +0200

    Create H08 opdrachten 2.java

---
+++ H08 opdrachten 2.java
@@ -0,0 +1,12 @@
+size(60, 280);
+background(255, 255, 255);
+for (int i = 0; i <= 10; i++) {
+  rect(20, i * 20 + 20, 20, 20);
+}
+for (int i = 0; i <= 10; i++) {
+  print(i * 20 + 20);
+  print(" | ");
+  print(i*20);
+  print(" | ");
+  println(i);
+}
