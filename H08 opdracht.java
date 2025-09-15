30e20783d8836994527542d508caf22fcf49a72c
DeonsGaming <Deon.mob@gmail.com>
2025-09-15 11:04:10 +0200

    Update H08 opdracht.java

--- H08 opdracht.java
+++ H08 opdracht.java
@@ -1,10 +1,8 @@
 int teller = 0;
-while(teller <= 10){
-println("ik ga net zolang door totdat teller groter is dan 10.");
+float leeftijd = 15;
+leeftijd = leeftijd * 365 * 24 * 60 * 60;
+while(teller <= leeftijd){
+println("ik ga net zo lang door tellen tot en met je leeftijd in seconden en dat is" + leeftijd);
 println("teller is nu " + teller);
 teller++;
-}
-De code hierboven gebeurt 11 keer want 0 tot en met 10 is elf getallen. Een trucje hiervoor
-wordt uitgelegd in H08.1.
-Opdracht 8.1
-Maak sketch met een while loop die 10 keer teller print in de console.
+
