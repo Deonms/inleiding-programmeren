839b65ed5bde839d6408ac25ff046231aa5cd56e
DeonsGaming <Deon.mob@gmail.com>
2025-09-15 10:45:40 +0200

    Update H07 eind opdracht.java

--- H07 eind opdracht.java
+++ H07 eind opdracht.java
@@ -25,21 +25,21 @@ switch(dagenVanDeWeek) {
 }
 
 int cijferResultaat = 1;
-if (cijferResultaat = 1 ||cijferResultaat = 2 || cijferResultaat = 3) {
+if (cijferResultaat == 1 ||cijferResultaat == 2 || cijferResultaat == 3) {
     println(cijferResultaat + " Resultaat is Slecht");
 }
-else if (cijferResultaat = 4) {
+else if (cijferResultaat == 4) {
     println(cijferResultaat + " Resultaat is Onvoldoende");
 }
-else if (cijferResultaat = 5) {
+else if (cijferResultaat == 5) {
     println(cijferResultaat + " Resultaat is Matig");
 }
-else if (cijferResultaat = 6 || cijferResultaat = 7) {
+else if (cijferResultaat == 6 || cijferResultaat == 7) {
     println(cijferResultaat + " Resultaat is Voldoende");
 }
-else if (cijferResultaat = 8 || cijferResultaat = 9 || cijferResultaat = 10) [
+else if (cijferResultaat == 8 || cijferResultaat == 9 || cijferResultaat == 10) {
     println(cijferResultaat + " Resultaat is Goed");
-]
+}
 else if (cijferResultaat >= 11) {
     println("Cijfer = " + cijferResultaat + " is Niet Geldig Controleer of je het goed hebt ingevuld");
 }
