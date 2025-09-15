e20bb6ff6370e13a0f9231838abcb44042fb91a4
DeonsGaming <Deon.mob@gmail.com>
2025-09-15 14:14:14 +0200

    Update H09.5 opdracht.java

--- H09.5 opdracht.java
+++ H09.5 opdracht.java
@@ -10,3 +10,15 @@ void draw() {
 void mijnMethode() {
   println(totaal);
 }
+
+void setup() {
+  mijnMethode(5, 6);
+}
+
+void draw () {
+}
+
+void mijnMethode(int getal, int getaltwee) {
+  int totaal = getal + getaltwee;
+  println("som "+ getal + "+" + getaltwee + "=" + totaal);
+}
