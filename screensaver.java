aeb0af2e21c4691d82fdf580d8720ad62d0a58e0
DeonsGaming <Deon.mob@gmail.com>
2025-09-16 09:41:45 +0200

    Update screensaver.java

--- screensaver.java
+++ screensaver.java
@@ -1,21 +1,23 @@
 int xWaarde = 0;
 int yWaarde = 0;
-void setup(){
-size(750,750);
+void setup() {
+  size(750, 750);
 }
-void draw(){
-//background(255,255,255);
-xWaarde++;
-yWaarde+=5;
-if(xWaarde >= 1000){
-xWaarde = 0;
-}
-if(yWaarde >= 1000){
-yWaarde = 0;
-}
-fill(255, 255, 255);
-  ellipse(xWaarde, yWaarde, 250, 250);
+void draw() {
+  //background(255,255,255);
+  xWaarde++;
+  yWaarde+=5;
+  if (xWaarde >= 900) {
+    xWaarde = 0;
+  }
+  if (yWaarde >= 900) {
+    yWaarde = 0;
+  }
   fill(0, 0, 0);
+  ellipse(xWaarde, yWaarde, 350, 350);
+  fill(0, 0, 0);
+  ellipse(xWaarde, yWaarde, 250, 250);
+  fill(255, 255, 255);
   ellipse(xWaarde, yWaarde, 200, 200);
   fill(0, 0, 255);
   ellipse(xWaarde, yWaarde, 150, 150);
