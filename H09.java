c195721d14d9545009a5f952dff35a3c7ce1bef9
DeonsGaming <Deon.mob@gmail.com>
2025-09-22 13:16:28 +0200

    Update H09.java

--- H09.java
+++ H09.java
@@ -5,7 +5,7 @@ float eindnummer;
 eindnummer = (nummer1 + nummer2) / 2 ;
 
 println(eindnummer);
-
+//andere stuk code van een andere opdracht
 void setup(){
   size(300, 300);
 }
@@ -14,4 +14,14 @@ void setup(){
   line(10, 230, 190, 230);
   line(10, 50, 10, 230);
   line(190, 230, 190, 50);
+}
+// andere opdracht
+size(400, 400);
+int eerste = 1;
+int tweede = 2;
+int derde = 3;
+int vierde = 0;
+while (vierde < 1000000000) {
+  println(vierde);
+  vierde = vierde + (vierde + derde + eerste) / tweede;
 }
