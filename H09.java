180145681208e08d7d373150a4d5abbf63b31d84
DeonsGaming <Deon.mob@gmail.com>
2025-09-22 13:20:47 +0200

    Update H09.java

--- H09.java
+++ H09.java
@@ -24,3 +24,19 @@ while (vierde < 1000000000) {
   println(vierde);
   vierde = vierde + (vierde + derde + eerste) / tweede;
 }
+// andere opdracht
+int eerste = 1;
+int tweede = 2;
+int derde = 3;
+int vierde = 0;
+while (vierde < 1000000000) {
+  println(vierde);
+  vierde = vierde + (vierde + derde + eerste) / tweede;
+size(200,200);
+background(255,255,255);
+int sizeC = 110;
+for(int i = 0; i < 5; i++){
+ellipse(110 - sizeC/2, 110 - sizeC/20, sizeC,sizeC);
+sizeC = sizeC - 10;
+}
+}
