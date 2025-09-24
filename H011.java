c4e346a863a5876caed74a95d002d204fa0e2f5c
DeonsGaming <Deon.mob@gmail.com>
2025-09-24 09:09:17 +0200

    Update H011.java

--- H011.java
+++ H011.java
@@ -1,3 +1,14 @@
 int aantal = 27;
 int[] variabeleNaam = new int[aantal];
 println(variabeleNaam);
+//new code
+int[] mijnStapelGetllen = {10, 5, 20, 7, 4 ,143, 2};
+println(mijnStapelGetllen);
+//new codes
+int aantal = 11;
+int[] variabeleNaam = new int[aantal];
+
+int[] mijnStapelGetllen = {10, 9, 8, 7, 6 ,5, 4, 3, 2, 1, 0};
+println(variabeleNaam);
+println(mijnStapelGetllen);
+// new code
