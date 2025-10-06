535de6c564cc45001fd0dbd5f7b4f64897561978
DeonsGaming <Deon.mob@gmail.com>
2025-10-06 11:12:53 +0200

    Update h12.java

--- h12.java
+++ h12.java
@@ -22,3 +22,18 @@ void setup() {
   Rectangle mijnRechthoek = new Rectangle(200, 200, 100, 175);
   mijnRechthoek.teken();
 }
+//nieuwe code en opdracht
+void setup(){
+  Person mijnPerson1 = new Person();
+  Person mijnPerson2 = new Person();
+  Person mijnPerson3 = new Person();
+  mijnPerson1.naam = "Deon";
+  mijnPerson2.age = 15;
+  mijnPerson3.gender = "Jongen";
+ println("Hallo mijn naam is: " + mijnPerson1.naam + " en ik ben " + mijnPerson2.age + " jaar oud en ik ben een " + mijnPerson3.gender);
+}
+class Person {
+  String naam;
+  int age;
+  String gender;
+}
